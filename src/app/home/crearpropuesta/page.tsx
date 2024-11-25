20081ea9e60a67f708a74e9bce15753500003e08
frank gimeno <frankgimeno3@gmail.com>
2024-11-25 20:46:35 +0100

    creo nuevas páginas como estructura del proyecto

---
+++ src/app/home/crearpropuesta/page.tsx
@@ -0,0 +1,191 @@
+'use client'
+import React, { FC, useState } from 'react';
+ import { addPropuesta, Product } from '@/lib/propuestas/addPropuesta';
+ import { useFormState, useFormStatus } from 'react-dom';
+import { Button } from '@/app/auth/signup/button';
+
+interface CrearPropuestaProps {}
+
+const CrearPropuesta: FC<CrearPropuestaProps> = ({ }) => {
+
+  // const [errorMessage, dispatch] = useFormState(handleSignUp, undefined);
+  const { pending } = useFormStatus();
+  
+  // Nuevos campos para la propuesta
+  const [lang, setLang] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [descuento, setDescuento] = useState(0);
+  const [impuesto, setImpuesto] = useState("");
+  const [numeroAgente, setNumeroAgente] = useState("");
+
+  // Estado para productos
+  const [products, setProducts] = useState<Product[]>([]);
+  
+  // Función para agregar un producto
+  const addProduct = () => {
+    setProducts([
+      ...products,
+      { productId: '', productComments: '', productPrice: '' }
+    ]);
+  };
+
+  // Función para manejar cambios en los productos
+  const handleProductChange = (index: number, field: keyof Product, value: string) => {
+    const updatedProducts = [...products];
+    updatedProducts[index] = { ...updatedProducts[index], [field]: value };
+    setProducts(updatedProducts);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);  
+
+    try {
+      await addPropuesta(lang, clientId, products, descuento, impuesto, numeroAgente);
+      
+    } catch (error) {
+      console.error("Error al crear la propuesta:", error);
+    }
+  };
+
+  return (
+    <div className='bg-gray-50 min-h-screen p-44 text-gray-600'>
+      <form onSubmit={handleSubmit} className="space-y-3 text-gray-600">
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+          <h1 className="mb-3 text-2xl">Crear propuesta</h1>
+
+          {/* Campos para lang, clientId, descuento, e impuesto */}
+          <div className="mt-4">
+            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="lang">
+              Idioma
+            </label>
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+              id="lang"
+              type="text"
+              value={lang}
+              onChange={(e) => setLang(e.target.value)}
+              placeholder="Introduce el idioma"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="clientId">
+              ID del cliente
+            </label>
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+              id="clientId"
+              type="text"
+              value={clientId}
+              onChange={(e) => setClientId(e.target.value)}
+              placeholder="Introduce el ID del cliente"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="descuento">
+              Descuento
+            </label>
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+              id="descuento"
+              type="number"
+              value={descuento}
+              onChange={(e) => setDescuento(Number(e.target.value))}
+              placeholder="Introduce el descuento"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="impuesto">
+              Impuesto
+            </label>
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+              id="impuesto"
+              type="string"
+              value={impuesto}
+              onChange={(e) => setImpuesto(e.target.value)}
+              placeholder="Introduce el impuesto"
+              required
+            />
+          </div>
+
+          {/* Campos para productos */}
+          {products.map((product, index) => (
+            <div key={index} className="mt-4 space-y-2">
+              <div>
+                <label className="mb-3 mt-5 block text-xs font-medium text-gray-900">ID del producto</label>
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+                  value={product.productId}
+                  onChange={(e) => handleProductChange(index, 'productId', e.target.value)}
+                  placeholder="Introduce el ID del producto"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="mb-3 mt-5 block text-xs font-medium text-gray-900">Comentarios del producto</label>
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+                  value={product.productComments}
+                  onChange={(e) => handleProductChange(index, 'productComments', e.target.value)}
+                  placeholder="Introduce comentarios sobre el producto"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="mb-3 mt-5 block text-xs font-medium text-gray-900">Precio del producto</label>
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+                  value={product.productPrice}
+                  onChange={(e) => handleProductChange(index, 'productPrice', e.target.value)}
+                  placeholder="Introduce el precio del producto"
+                  required
+                />
+              </div>
+            </div>
+          ))}
+
+          <Button type="button" className="mt-4 w-full" onClick={addProduct}>
+            Agregar producto
+          </Button>
+
+          <div className="mt-4">
+            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="numeroAgente">
+              Número del agente
+            </label>
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2"
+              id="numeroAgente"
+              type="text"
+              value={numeroAgente}
+              onChange={(e) => setNumeroAgente(e.target.value)}
+              placeholder="Introduce el número del agente"
+              required
+            />
+          </div>
+
+         
+
+          <Button className="mt-4 w-full" aria-disabled={pending}>
+            Crear propuesta
+          </Button>
+
+          {/* <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
+            {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
+          </div> */}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CrearPropuesta;
---
+++ src/app/home/propuestas/page.tsx
@@ -0,0 +1,78 @@
+import React, { FC } from 'react';
+
+interface PropuestasProps {}
+
+const Propuestas: FC<PropuestasProps> = () => {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">
+        Aquí se deben ver todas las propuestas
+      </h1>
+      <p className="text-gray-600 mb-6">Quiero ver propuestas de...</p>
+      
+      {/* Filtros */}
+      <div className="bg-white p-4 shadow-md rounded-md mb-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {/* Filtro por Agente */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-2">Agente:</label>
+            <select className="w-full p-2 border border-gray-300 rounded-md">
+              <option>una opción por cada agente</option>
+            </select>
+          </div>
+
+          {/* Filtro por Cliente */}
+          <div>
+            <label className="block text-gray-700 font-medium mb-2">Cliente:</label>
+            <input
+              type="text"
+              placeholder="Inserte número de cliente"
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+        </div>
+        <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+          Buscar
+        </button>
+      </div>
+
+      {/* Tabla de Resultados */}
+      <div className="bg-white p-4 shadow-md rounded-md">
+        <p className="text-gray-700 mb-4">
+          Aquí debe ir una tabla con columnas "num cliente", "nombre agente", "nombre propuesta", eliminar
+        </p>
+        <table className="w-full table-auto border-collapse">
+          <thead>
+            <tr className="bg-gray-200 text-gray-700">
+              <th className="border px-4 py-2">Nombre Cliente</th>
+              <th className="border px-4 py-2">Nombre Agente</th>
+              <th className="border px-4 py-2">Nombre Propuesta</th>
+              <th className="border px-4 py-2">Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Filas de ejemplo */}
+            <tr className="hover:bg-gray-100">
+              <td className="border px-4 py-2 text-center">LISEC</td>
+              <td className="border px-4 py-2 text-center">Frank Gimeno</td>
+              <td className="border px-4 py-2 text-center">Propuesta 1 - 2024</td>
+              <td className="border px-4 py-2 text-center">
+                <button className="text-red-500 hover:underline">Eliminar</button>
+              </td>
+            </tr>
+            <tr className="hover:bg-gray-100">
+              <td className="border px-4 py-2 text-center">SITEC</td>
+              <td className="border px-4 py-2 text-center">Rosa Ruiz</td>
+              <td className="border px-4 py-2 text-center">Propuesta 2 - 2024</td>
+              <td className="border px-4 py-2 text-center">
+                <button className="text-red-500 hover:underline">Eliminar</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Propuestas;
